2433ec8482af2b927160263d243637475d8f1d66
Quang Dai <trinhdai5903@gmail.com>
2024-04-24 16:54:38 +0700

    router, react-router-dom config

---
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            Header Components
+        </div>
+    )
+}
+
+export default HeaderComponent;
