441e0667c0d226c2a35f6b03e93b783acd40c388
Quang Dai <trinhdai5903@gmail.com>
2024-05-13 12:53:44 +0700

    update user api

---
+++ back-end/src/services/JwtService.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const generalAccessToken = (payload) => {
+  console.log("payload : ", payload);
+  const access_token = jwt.sign(
+    {
+      payload,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: "1h" }
+  );
+  return access_token;
+};
+
+const generalRefreshToken = (payload) => {
+  console.log("payload : ", payload);
+  const refresh_token = jwt.sign(
+    {
+      payload,
+    },
+    process.env.REFRESH_TOKEN,
+    { expiresIn: "365d" }
+  );
+  return refresh_token;
+};
+
+module.exports = {
+  generalAccessToken,
+  generalRefreshToken,
+};
--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const { generalAccessToken, generalRefreshToken } = require("./JwtService");
 const createUser = (newUser) => {
   return new Promise(async (resolve, reject) => {
     const { name, email, password, confirmPassword, phone } = newUser;
@@ -12,11 +14,14 @@ const createUser = (newUser) => {
           message: "The email is Already",
         });
       } else {
+        const hash = bcrypt.hashSync(password, 10);
+        // ma hoa ket hop voi password truyen len va ma hoa
+        // ra dang ki tu dac biet,
+        console.log("hash : ", hash);
         const createUser = await User.create({
           name,
           email,
-          password,
-          confirmPassword,
+          password: hash,
           phone,
         });
         if (createUser) {
@@ -32,6 +37,80 @@ const createUser = (newUser) => {
     }
   });
 };
+const loginUser = (userLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { name, email, password, confirmPassword, phone } = userLogin;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "The user is not defined",
+        });
+      } else {
+        const comparePassword = bcrypt.compareSync(
+          password,
+          checkUser.password
+        );
+        if (!comparePassword) {
+          resolve({
+            status: "OK",
+            message: "The password or user is incorrect",
+          });
+        }
+        const access_token = await generalAccessToken({
+          id: checkUser.id,
+          isAdmin: checkUser.isAdmin,
+        });
+
+        const refresh_token = await generalRefreshToken({
+          id: checkUser.id,
+          isAdmin: checkUser.isAdmin,
+        });
+        console.log("access_token : ", access_token);
+        resolve({
+          status: "OK",
+          message: "SUCCESS",
+          access_token,
+          refresh_token,
+        });
+      }
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
+
+const updateUser = (id, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({
+        _id: id,
+      });
+      console.log("checkUser : ", checkUser);
+      if (checkUser === null) {
+        resolve({
+          status: "OK",
+          message: "The user is not defined",
+        });
+      }
+      const updateUser = await User.findByIdAndUpdate(id, data, { new: true });
+      console.log("update user : ", updateUser);
+      resolve({
+        status: "OK",
+        message: "SUCCESS",
+        data: updateUser,
+      });
+      // }
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
 module.exports = {
   createUser,
+  loginUser,
+  updateUser,
 };
